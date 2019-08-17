8271644007a16998180fb5e2ab8f8100ed8d2ae0
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-16 21:54:23 -0400

    Add boilerplate for the server

---
+++ index.js
@@ -0,0 +1,4 @@
+const server = require('./src/server');
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.use('/api', require('./routes'));
+
+module.exports = server;
